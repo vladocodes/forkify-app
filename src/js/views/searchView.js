d99a0c5757291360ce4deb690cbe4f62a9d3db9f
Vladimir Mijic <vlada.work.and.business@gmail.com>
2020-09-17 08:49:22 +0200

    first commit

---
+++ src/js/views/searchView.js
@@ -0,0 +1,82 @@
+import { elements }  from './base';
+
+export const getInput = () => elements.searchInput.value;
+export const clearInput = () => { elements.searchInput.value = ''; };
+export const clearResults = () => { 
+    elements.searchResultList.innerHTML = '';
+    elements.searchResultsPages.innerHTML = '';
+};
+export const highlightSelected = id => {
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(el => {
+        el.classList.remove('results__link--active');
+    });
+    document.querySelector(`.results__link[href="#${id}"]`).classList.add('results__link--active');
+};
+
+
+export const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((accumulator, current) => {
+            if (accumulator + current.length <= limit) {
+                newTitle.push(current);
+            }
+            return accumulator + current.length;
+        }, 0);
+        return `${newTitle.join(' ')}...`;
+    } 
+    return title;
+}
+
+const renderRecipe = recipe => {
+    const markup = `
+    <li>
+        <a class="results__link results__link--active" href="#${recipe.recipe_id}">
+            <figure class="results__fig">
+                <img src="${recipe.image_url}" alt="Test">
+            </figure>
+            <div class="results__data">
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                <p class="results__author">${recipe.publisher}</p>
+            </div>
+        </a>
+    </li>
+    `;
+    elements.searchResultList.insertAdjacentHTML('beforeend', markup);
+};
+
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page -1 : page + 1}>
+        <span>Page ${type === 'prev' ? page -1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
+
+const renderButtons = (page, numResults, resultsPerPage = 10) => {
+    const pages = Math.ceil(numResults / resultsPerPage);
+
+    let button;
+    if (page === 1 && pages > 1) {
+        button = createButton(page, 'next');
+    } else if (page < pages) {
+        button = `
+        ${createButton(page, 'prev')}
+        ${createButton(page, 'next')}
+        `;
+    } else if (page === pages && pages > 1) {
+        button = createButton(page, 'prev');
+    }
+    elements.searchResultsPages.insertAdjacentHTML('afterbegin', button);
+};
+
+export const renderResults = (recipes, page = 1, resultsPerPage = 10) => {
+    const start = (page - 1) * resultsPerPage;
+    const end = page * resultsPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    renderButtons(page, recipes.length, resultsPerPage);
+};
